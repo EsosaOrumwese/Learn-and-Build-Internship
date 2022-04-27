cebc352cb9196a24700932d85c7fb7ef9b6e6223
Esosa Orumwese <67744536+EsosaOrumwese@users.noreply.github.com>
2022-04-27 15:17:31 +0100

    Done with functions module

---
+++ Pre-Data Science/testbay.py
@@ -0,0 +1,12 @@
+# import subprocess, time
+# cmd = input("Enter any command : ")
+# out = subprocess.getstatusoutput(cmd)
+# if out[0] == 0:
+#     print("Command Found")
+#     time.sleep(1)
+#     print(out)
+# else:
+#     print("Command not found")
+
+
+
